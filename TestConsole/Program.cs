0110a9f2b79d20ff30b34efdb072697174fb9237
Alex Mopan <work@malex.info>
2025-06-05 17:07:13 +0300

    changes in code

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Super home task!");
 var i=0;
-Console.WriteLine("i="+i);
-i++;
+
+while (i < 10)
+{
+    Console.WriteLine("i=" + i);
+
+    i++;
+}
+
 
